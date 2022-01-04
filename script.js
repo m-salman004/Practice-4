234bac7d5095917c8e92ab6e59915c52882310dc
Muhammad Salman <92182319+m-salman004@users.noreply.github.com>
2022-01-04 11:19:16 -0800

    Toastify Error

--- script.js
+++ script.js
@@ -14,7 +14,23 @@ function clearOutput(){
 function showOutput(outputResult){
     document.getElementById("outputResult").innerHTML += outputResult
 }
-
+// Toastify Error
+function error(innerText){
+    Toastify({
+        text: innerText,
+        duration: 3000,/* 
+        destination: "https://github.com/apvarun/toastify-js",
+        newWindow: true, */
+        close: true,
+        gravity: "top", // `top` or `bottom`
+        position: "center", // `left`, `center` or `right`
+        stopOnFocus: true, // Prevents dismissing of toast on hover
+        style: {
+          background: "linear-gradient(to right, #00b09b, #96c93d)",
+        },
+        onClick: function(){} // Callback after click
+      }).showToast();
+}
 
 
 
